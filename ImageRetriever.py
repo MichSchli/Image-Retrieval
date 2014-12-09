b8e933cb045702b38ac34e50e1e4a2aca6ec03e4
MichSchli <michael.sejr@gmail.com>
2014-12-09 10:56:34 +0100

    matching
    some awesome matching

--- ImageRetriever.py
+++ ImageRetriever.py
@@ -2,7 +2,31 @@ __author__ = 'Michael'
 '''
 Imports:
 '''
-import Codebook.py
+import Codebook
+
+'''
+Image matching algorithm:
+'''
+
+#Define a function to find the best match for an image given a metric
+def find_best_match(image_index, bow_list, metric):
+    best = -1
+    best_distance = -1
+
+    #Iterate over all possible matches:
+    for i in xrange(len(bow_list)):
+        #Don't match an image to itself:
+        if image_index != i:
+            #Calculate the distance:
+            d = metric(bow_list[image_index], bow_list[i])
+
+            #If the new distance is better, memorize it:
+            if best_distance == -1 or best_distance > d:
+                best = i
+                best_distance = d
+
+    #Return the best match:
+    return best
 
 '''
 Interface:
@@ -13,4 +37,10 @@ Testing:
 '''
 
 if __name__ == '__main__':
-    pass
+    cb = Codebook.construct_table(10, limit=4)
+
+    bows = [x[1] for x in cb]
+
+    img = 0
+
+    print find_best_match(img, bows, lambda x,y: 1)
